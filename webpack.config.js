2c14ba2b6a79e5a556b7e5fcd0125bc1d54878c4
LadaMocherniuk <ladamochernyuk@gmail.com>
2021-08-17 14:59:08 +0300

    new1

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+    entry:'./src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+            inject: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+            
+        })
+    ],
+    devServer: {
+        port: 4000
+    },
+    devtool: 'cheap-eval-source-map'
+    
+};
